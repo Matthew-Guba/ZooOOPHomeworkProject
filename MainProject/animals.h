f7df0025e03731260c45c97e0718eea05549308b
Matthew-Guba <chemistrygeorge3@gmail.com>
2025-04-06 14:15:31 -0700

    Add project files.

---
+++ MainProject/animals.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal {
+	
+public:
+	string name;
+	int age;
+	double weight;
+	bool alive;
+	bool is_predator;
+};
---
+++ MainProject/main.cpp
@@ -0,0 +1,59 @@
+#include "animals.h"
+
+int main() {
+
+	int quanity;
+
+	cout << "Enter the number of animals: ";
+	cin >> quanity;
+
+	if (quanity <= 0) {
+		cout << "Invalid data.";
+		return 0;
+	}
+
+	Animal* an = new Animal[quanity];
+
+	for (int i = 0; i <= quanity; i++) {
+		cout << "Enter data of " << i + 1 << " animal: " << endl;
+
+		cout << "Name: ";
+		cin >> an[i].name;
+
+		cout << "Age: ";
+		cin >> an[i].age;
+
+		cout << "Weight: ";
+		cin >> an[i].weight;
+
+		string alive;
+		cout << "Alive or not(type yes or not): ";
+		cin >> alive;
+
+
+		if (alive == "yes") {
+			an[i].alive = true;
+		}
+		else{
+			an[i].alive = false;
+		}
+		
+
+		string is_predator;
+		cout << "Is animal a predator(type yes or not): ";
+		cin >> an[i].is_predator;
+
+
+		if (is_predator == "yes") {
+			an[i].is_predator = true;
+		}
+		else {
+			an[i].is_predator = false;
+		}
+		
+		cout << endl;
+	}
+
+
+	return 0;
+}
