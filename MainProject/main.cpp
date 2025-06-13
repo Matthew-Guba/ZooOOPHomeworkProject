1af513f628bde05973a0642cbe15eb21ce9fce70
matthew-guba <chemistrygeorge3@gmail.com>
2025-06-13 17:24:31 +0300

    update

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -9,7 +9,6 @@
 int main() {
     Zoo zoo;
 
-    // Add all animals to the zoo
     zoo.addAnimal(new Lion("Simba", 5, 190.5, 25.0));
     zoo.addAnimal(new Tiger("Tony", 8, 220.0, 7.5));
     zoo.addAnimal(new Elephant("Dumbo", 15, 6000.0, 120.5));
@@ -17,7 +16,6 @@ int main() {
     zoo.addAnimal(new Giraffe("Melman", 12, 800.0, 2.1));
     zoo.addAnimal(new Zebra("Marty", 7, 350.0, 42));
 
-    // Display zoo information
     zoo.listAnimals();
     zoo.makeAllSounds();
 
